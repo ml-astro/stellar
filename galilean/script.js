488020ffff5437192da76c8ff7a25f1971a62c7e
Max <maksim.grit299@slmail.me>
2025-03-02 22:28:56 +0200

    galilean better xy calc

--- galilean/script.js
+++ galilean/script.js
@@ -15,9 +15,11 @@ const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
 /* WORKS BAD WITH CIRCULAR ORBITS */
-const today = new Date()
+//const today = new Date()
+const today = new Date(2026,2,14,8,40)
 //UTC
-const referenceDate = new Date(2024, 8, 22, 14, 33)
+//const referenceDate = new Date(2024, 8, 22, 14, 33)
+const referenceDate = new Date(2025, 0, 1, 0, 0)
 var Ndays = ((today - referenceDate) / 86400000)
 
 function toRadians(degrees) {
@@ -27,7 +29,7 @@ function toRadians(degrees) {
 function toDegrees(radians) {
     return radians * (180 / Math.PI)
 }
-
+/*
 const Jupiter = {
     sma: 5.2038,
     offset: 69.568,
@@ -39,15 +41,58 @@ const Earth = {
     offset: 0.0,
     daily: 360 / 365.256363004
 }
+*/
 
-Earth.longitude = Earth.offset + Ndays * Earth.daily % 360
+const Jupiter = {
+    num: 3,
+        N: 100.52021,
+        i: 1.30346,
+        w: 273.609,
+        a: 5.202827,
+        e: 0.0483062,
+        M: 58.982826,
+        dM: 0.08309065
+}
+
+const Earth = {
+        N: 190.71637577,
+        i: 0.00297708,
+        w: 272.9783,
+        a: 1.00091255,
+        e: 0.0175619,
+        M: 357.412225,
+        dM: 0.984262
+}
+
+calculatePosition(Earth)
+calculatePosition(Jupiter)
+
+function calculatePosition(planet) {
+    let N = planet.N// + planet.dN * d //Long of asc. node    
+    let i = planet.i// + planet.di * d //Inclination
+    let w = planet.w// + planet.dw * d //Argument of perihelion
+    let a = planet.a //Semi-major axis
+    let e = planet.e// + planet.de * d //Eccentricity
+    //put the updated numbers for dM later from horizons interface
+    let M = (planet.M + planet.dM * Ndays) % 360 + 360 //Mean anonaly
+    let E0 = M + (180 / Math.PI) * e * Math.sin(toRadians(M)) * (1.0 + e * Math.cos(toRadians(M))) //eccentric anomaly
+    let E1 = E0 - (E0 - (180 / Math.PI) * e * Math.sin(toRadians(E0)) - M) / (1.0 - e * Math.cos(toRadians(E0))) //eccentric anomaly
+    let v = toDegrees(2.0 * Math.atan(Math.sqrt((1.0 + e) / (1.0 - e)) * Math.tan(toRadians(E1) / 2.0))); //true anomaly
+    planet.r = (a * (1.0 - e * e)) / (1.0 + e * Math.cos(toRadians(v))); //current distance from sun
+
+    planet.x = planet.r * (Math.cos(toRadians(N)) * Math.cos(toRadians(v + w)) - Math.sin(toRadians(N)) * Math.sin(toRadians(v + w)) * Math.cos(toRadians(i)))
+    planet.y = planet.r * (Math.sin(toRadians(N)) * Math.cos(toRadians(v + w)) + Math.cos(toRadians(N)) * Math.sin(toRadians(v + w)) * Math.cos(toRadians(i)))
+    planet.longitude = (toDegrees(Math.atan2(planet.y, planet.x)) + 360) % 360
+}
+
+/*Earth.longitude = Earth.offset + Ndays * Earth.daily % 360
 Jupiter.longitude = Jupiter.offset + Ndays * Jupiter.daily % 360
 calculateXY(Earth)
 calculateXY(Jupiter)
-
+*/
 /* Calculating X,Y positions */
 
-function calculateXY(planet) {
+function calculateXY(planet) {   
     planet.longitude = planet.longitude % 360
     if (planet.longitude < 90) {
         planet.x = planet.sma * Math.cos(toRadians(planet.longitude))
@@ -110,25 +155,25 @@ const moons = [
         name: 'Io',
         sma: 1.0,
         period: 1.769137786,
-        offset: 292.8,
+        offset: 218.8,
     },
     {
         name: 'Europa',
         sma: 1.5909,
         period: 3.551181,
-        offset: 254.38,
+        offset: 4.88,
     },
     {
         name: 'Ganymede',
         sma: 2.5383,
         period: 7.15455296,
-        offset: 326.27,
+        offset: 348.28,
     },
     {
         name: 'Callisto',
         sma: 4.4645,
         period: 16.6890184,
-        offset: 136.94,
+        offset: 149.75,
     }
 ]
 
