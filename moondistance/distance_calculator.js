9a1c43f158214b041782f4ce9bc03c8306cae7f0
Maksim <maksim.ljovin@tutamail.com>
2026-03-29 16:07:12 +0300

    distance result in table

--- moondistance/distance_calculator.js
+++ moondistance/distance_calculator.js
@@ -7,9 +7,9 @@ const g = 9.81
 //const G = 6.67430e-11
 //const M_EARTH = 5.9722e24
 
-function toDegrees(rad) { return rad * 180 / Math.PI; }
-function toRadians(deg) { return deg * Math.PI / 180; }
-function vectorMagnitude(v) { return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z); }
+function rad2deg(rad) { return rad * 180 / Math.PI; }
+function deg2rad(deg) { return deg * Math.PI / 180; }
+function magnitude(v) { return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z); }
 
 function angularSeparation(ra1, dec1, ra2, dec2) {
     x = Math.cos(dec1) * Math.sin(dec2) - Math.sin(dec1) * Math.cos(dec2) * Math.cos(ra2 - ra1);
@@ -28,8 +28,8 @@ function calculateLST(longitude, JD, T) {
 }
 
 function observerVector(latDeg, lstDeg) {
-    const lat = toRadians(latDeg);
-    const lst = toRadians(lstDeg);
+    const lat = deg2rad(latDeg);
+    const lst = deg2rad(lstDeg);
     return {
         x: R_EARTH * Math.cos(lst) * Math.cos(lat),
         y: R_EARTH * Math.sin(lst) * Math.cos(lat),
@@ -42,7 +42,7 @@ function dot(a, b) {
 }
 
 function angleBetween(a, b) {
-    const cos = dot(a, b) / (vectorMagnitude(a) * vectorMagnitude(b));
+    const cos = dot(a, b) / (magnitude(a) * magnitude(b));
     return Math.acos(cos); // в радианах
 }
 
@@ -54,9 +54,10 @@ function sub(a, b) {
     };
 }
 
+//расчитывает единичный или полный вектор
 function direction(raDeg, decDeg, length = 1) {
-    const ra = toRadians(raDeg);
-    const dec = toRadians(decDeg);
+    const ra = deg2rad(raDeg);
+    const dec = deg2rad(decDeg);
     return {
         x: length * Math.cos(ra) * Math.cos(dec),
         y: length * Math.sin(ra) * Math.cos(dec),
@@ -65,54 +66,58 @@ function direction(raDeg, decDeg, length = 1) {
 }
 
 function calculateDistance(observationDate, observation) {
-    console.log('observation 1', observation[0]);
-    console.log('observation 2', observation[1]);
     JD = observationDate / 1000 / 3600 / 24 + 2440587.5
     T = (JD - 2451545.0) / 36525
-    parallax = angularSeparation(toRadians(observation[0].ra), toRadians(observation[0].dec), toRadians(observation[1].ra), toRadians(observation[1].dec))
+    parallax = angularSeparation(deg2rad(observation[0].ra), deg2rad(observation[0].dec), deg2rad(observation[1].ra), deg2rad(observation[1].dec))
     lst1 = calculateLST(observation[0].lon, JD, T)
     lst2 = calculateLST(observation[1].lon, JD, T)
-    hours = (lst2) / 360 * 24
-    console.log('lst1', lst1, 'lst2', lst2);
     const rL1 = observerVector(observation[0].lat, lst1);
     const rL2 = observerVector(observation[1].lat, lst2);
     const rL2L1 = sub(rL2, rL1);
-    const chord = vectorMagnitude(rL2L1);
-    
+    const chord = magnitude(rL2L1);
     //Now that vector rL2L1 is determined, the directional vector rM needs to be calculated
-    //расчитывает единичный или полный вектор
     const rM = direction(observation[1].ra, observation[1].dec);
-    console.log('rL1',rL1,'rL2',rL2,'rL2L1',rL2L1,'chord',chord,'rM',rM);
     //Now use the dot product method for computing angle between rM and rL2L1:
     const beta = angleBetween(rL2L1, rM);
     baseline = chord * Math.sin(beta)
     distance = baseline / Math.sin(parallax)
-    vectorFromL1 = direction(observation[0].ra, observation[0].dec, distance)
-    orbitComponents = {
-        x: (rL1.x + vectorFromL1.x),
-        y: (rL1.y + vectorFromL1.y),
-        z: (rL1.z + vectorFromL1.z),
+
+    objectDirection = direction(observation[0].ra, observation[0].dec, distance)
+    objectXYZ = {
+        x: (rL1.x + objectDirection.x),
+        y: (rL1.y + objectDirection.y),
+        z: (rL1.z + objectDirection.z),
     }
-    orbitRadius = vectorMagnitude(orbitComponents)
+    orbitRadius = magnitude(objectXYZ)
     orbitG = g / ((orbitRadius / R_EARTH) ** 2);
+
     period = Math.sqrt(4 * Math.PI * Math.PI * orbitRadius / orbitG) / 60
-    periodString = 'мин'
-    if(period > 90){
-        period=period/60
-        periodString = 'час'
+    periodString = ''
+    periodDays = Math.floor(period / 1440)
+    if (periodDays > 0) {
+        periodString += periodDays + ' сут '
+        period -= periodDays * 1440
     }
-    else if(period > 24*60){
-        period = period/60/24
-        periodString = 'сут'
+    periodHours = Math.floor(period / 60)
+    if (periodHours > 0) {
+        periodString += periodHours + ' час '
+        period -= periodHours * 60
     }
-
-    document.getElementById('result').innerHTML = "Расстояние до объекта:<br>~ " + Math.floor(distance / 1000) + " км"
-    document.getElementById('extrainfo').innerHTML = `
-    <p>Высота орбиты: ${Math.round((orbitRadius - R_EARTH) / 1000)} км</p>
-    <p>Период обращения: ${Math.round(period*100)/100} ${periodString}</p>
-    <p>Расстояние между наблюдателями: ${Math.floor(chord / 1000)} км</p>
-    <p>Базис: ${Math.floor(baseline / 1000)} км</p>
-    <p>Параллакс: ${Math.round(toDegrees(parallax) * 100) / 100} градусов</p>
+    periodString += Math.floor(period) + ' мин'
+    
+    document.getElementById('result').innerHTML = `<tr><td>Расстояние:</td><td>${Math.floor(distance / 1000)} км</td></tr>
+    <tr><td>Высота орбиты:</td><td>${Math.round((orbitRadius - R_EARTH) / 1000)} км</td></tr>
+    <tr><td>Период обращения:</td><td>${periodString}</td></tr>
+    <tr><td>Расстояние между наблюдателями:</td><td>${Math.floor(chord / 1000)} км</td></tr>
+    <tr><td>Базис:</td><td>${Math.floor(baseline / 1000)} км</td></tr>
+    <tr><td>Параллакс:</td><td>${Math.round(rad2deg(parallax) * 100) / 100} градусов</td></tr>
     `
-
+    document.getElementById('extra').innerHTML =`
+    Наблюдение 1: ${JSON.stringify(observation[0])}
+    Наблюдение 2: ${JSON.stringify(observation[1])}
+    LST1: ${lst1} LST2: ${lst2}
+    rL1 ${JSON.stringify(rL1)}
+    rL2 ${JSON.stringify(rL2)}
+    rL2L1 ${JSON.stringify(rL2L1)}
+    `
 }
