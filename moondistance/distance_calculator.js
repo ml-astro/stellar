a2b216f74270d585131efb70ef3e5d8a280721d3
Max <maksim.grit299@slmail.me>
2026-04-05 11:27:44 +0300

    objvector

--- moondistance/distance_calculator.js
+++ moondistance/distance_calculator.js
@@ -82,12 +82,11 @@ function calculateDistance(observationDate, observation) {
     baseline = chord * Math.sin(beta)
     distance = baseline / Math.sin(parallax)
 
-    objVector = direction(observation[0].ra, observation[0].dec, distance)
-    
+    objectDirection = direction(observation[0].ra, observation[0].dec, distance)
     objectXYZ = {
-        x: (rL1.x + objVector.x),
-        y: (rL1.y + objVector.y),
-        z: (rL1.z + objVector.z),
+        x: (rL1.x + objectDirection.x),
+        y: (rL1.y + objectDirection.y),
+        z: (rL1.z + objectDirection.z),
     }
     orbitRadius = magnitude(objectXYZ)
     orbitG = g / ((orbitRadius / R_EARTH) ** 2);
