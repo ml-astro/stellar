d441967291e3307c5cd54f4dfed9a67dfd63a7eb
Maksim <maksim.ljovin@tutamail.com>
2026-04-07 16:25:17 +0300

    utilities rad deg

--- utilities/utils.js
+++ utilities/utils.js
@@ -17,14 +17,20 @@ function calculateLST(LSTform) {
     document.getElementById("LST").innerHTML = `
     Местное звездное время:
                             ${LST}&#176;
-    или                     ${LST/360*24} ч
+    или                     ${LST / 360 * 24} ч
 
     Юлианская дата:         ${JD}`
 }
 
 //градусы и радианы
-function rad2deg(rad) { return rad * 180 / Math.PI; }
-function deg2rad(deg) { return deg * Math.PI / 180; }
+function rad2deg(radForm) {
+    rad = radForm.radians.value
+    document.getElementById('degreesFromRadians').value = rad * 180 / Math.PI;
+}
+function deg2rad(degForm) {
+    deg = degForm.degrees.value
+    document.getElementById('radiansFromDegrees').value = deg * Math.PI / 180;
+}
 
 //угловое расстояние
 function angularSeparation(ra1, dec1, ra2, dec2) {
