76208910aa317d5bd77630ff343b36e9b2a2f6d3
Maksim <maksim.ljovin@tutamail.com>
2026-05-01 19:47:26 +0300

    hr2deg

--- utilities/utils.js
+++ utilities/utils.js
@@ -23,26 +23,26 @@ function calculateLST(LSTform) {
 }
 
 //градусы и радианы
-function rad2deg(radForm) {
-    rad = radForm.radians.value
-    document.getElementById('degreesFromRadians').value = rad * 180 / Math.PI;
+function rad2deg(form) {
+    rad = form.radians.value
+    form.degreesFromRadians.value = rad * 180 / Math.PI;
 }
-function deg2rad(degForm) {
-    deg = degForm.degrees.value
-    document.getElementById('radiansFromDegrees').value = deg * Math.PI / 180;
+function deg2rad(form) {
+    deg = form.degrees.value
+    form.radiansFromDegrees.value = deg * Math.PI / 180;
 }
 
 
 //угловое расстояние
-function toRadians(value){
+function toRadians(value) {
     return value * Math.PI / 180;
 }
 function angularSeparation(angleForm) {
-    ra1 = toRadians(toDecimalDegrees(angleForm.ra1.value,true))
-    ra2 = toRadians(toDecimalDegrees(angleForm.ra2.value,true))
+    ra1 = toRadians(toDecimalDegrees(angleForm.ra1.value, true))
+    ra2 = toRadians(toDecimalDegrees(angleForm.ra2.value, true))
     dec1 = toRadians(toDecimalDegrees(angleForm.dec1.value))
     dec2 = toRadians(toDecimalDegrees(angleForm.dec2.value))
-    console.log(ra1,ra2,dec1,dec2);
+    console.log(ra1, ra2, dec1, dec2);
     x = Math.cos(dec1) * Math.sin(dec2) - Math.sin(dec1) * Math.cos(dec2) * Math.cos(ra2 - ra1);
     y = Math.cos(dec2) * Math.sin(ra2 - ra1);
     z = Math.sin(dec1) * Math.sin(dec2) + Math.cos(dec1) * Math.cos(dec2) * Math.cos(ra2 - ra1);
@@ -51,7 +51,7 @@ function angularSeparation(angleForm) {
 }
 
 //часы в градусы
-function toDecimalDegrees(value,ra=false) {
+function toDecimalDegrees(value, ra = false) {
     if (isNaN(value)) {
         degArray = value.split(":")
         seconds = 0
@@ -59,57 +59,77 @@ function toDecimalDegrees(value,ra=false) {
             seconds = degArray[2] / 3600
         }
         if (degArray[0] < 0) {
-            if(ra){
-                return 15*(parseInt(degArray[0]) - degArray[1] / 60 - seconds)
+            if (ra) {
+                return 15 * (parseInt(degArray[0]) - degArray[1] / 60 - seconds)
             }
             else return parseInt(degArray[0]) - degArray[1] / 60 - seconds
         }
-        if(ra){
-            return 15*(parseInt(degArray[0]) + degArray[1] / 60 + seconds)
+        if (ra) {
+            return 15 * (parseInt(degArray[0]) + degArray[1] / 60 + seconds)
         }
-        else  return parseInt(degArray[0]) + degArray[1] / 60 + seconds
+        else return parseInt(degArray[0]) + degArray[1] / 60 + seconds
     }
     else return value
 }
 
 function refraction(form) {
-	var alt = parseFloat(document.getElementById('altHor').value);
-	if (alt < 0) {
-		document.getElementById('ref').value = 'Ниже горизонта';
-	}
-	else if (alt > 80) {
-		document.getElementById('ref').value = 0 + "'";
-	}
-	else if (alt > 15 && alt <= 80) {
-		alt = alt / 57.29577951308232;
-		var refraction = 271.2 / (283 * Math.tan(alt));
-		document.getElementById('ref').value = refraction + "'";
-	}
-	else {
-		subRef = 283 * (1.0 + 0.505 * alt + 0.0845 * alt * alt);
-		refraction = 60000 * (0.1594 + 0.0196 * alt + 0.00002 * alt * alt) / subRef;
-		document.getElementById('ref').value = refraction + "'";
-	}
-	var ok = true;
-	var height = document.getElementById('altHor').value;
-	var v = document.getElementById('altObs').value;
-	if (height < 0) {
-		document.getElementById('extinction').value = 'Ниже горизонта';
-		ok = false;
-	}
-	if (height > 90) {
-		height = 90 - (90 - height);
-		ok = false;
-	}
-	if (ok) {
-		var x1 = 0;
-		x1 = (1 / (Math.cos((90 - height) * 0.01745329251) + 0.025 * Math.exp(-11 * Math.cos((90 - height) * 0.01745329251))));
-		x2 = (0.1451 * Math.exp(-v * 0.001 / 7.996)) * x1;
-		x3b = (0.12 * Math.exp(-v * 0.001 / 1.5)) * x1;
-		x4 = 0.016 * x1;
-		document.getElementById('extinction').value = (x2 + x3b + x4) + 'm';
-	}
+    var alt = parseFloat(form.altHor.value);
+    if (alt < 0) {
+        form.ref.value = 'Ниже горизонта';
+    }
+    else if (alt > 80) {
+        form.ref.value = 0 + "'";
+    }
+    else if (alt > 15 && alt <= 80) {
+        alt = alt / 57.29577951308232;
+        var refraction = 271.2 / (283 * Math.tan(alt));
+        form.ref.value = refraction + "'";
+    }
+    else {
+        subRef = 283 * (1.0 + 0.505 * alt + 0.0845 * alt * alt);
+        refraction = 60000 * (0.1594 + 0.0196 * alt + 0.00002 * alt * alt) / subRef;
+        form.ref.value = refraction + "'";
+    }
+    var ok = true;
+    var height = form.altHor.value;
+    var v = form.altObs.value;
+    if (height < 0) {
+        form.extinction.value = 'Ниже горизонта';
+        ok = false;
+    }
+    if (height > 90) {
+        height = 90 - (90 - height);
+        ok = false;
+    }
+    if (ok) {
+        var x1 = 0;
+        x1 = (1 / (Math.cos((90 - height) * 0.01745329251) + 0.025 * Math.exp(-11 * Math.cos((90 - height) * 0.01745329251))));
+        x2 = (0.1451 * Math.exp(-v * 0.001 / 7.996)) * x1;
+        x3b = (0.12 * Math.exp(-v * 0.001 / 1.5)) * x1;
+        x4 = 0.016 * x1;
+        form.extinction.value = (x2 + x3b + x4) + 'm';
+    }
 }
 
+
+function time2degrees(form) {
+    timeArray = form.time.value.split(":")
+    seconds = 0
+    if (timeArray.length == 3) {
+        seconds = timeArray[2] / 3600
+    }
+    result = parseInt(timeArray[0]) + timeArray[1] / 60 + seconds
+    form.degreesFromTime.value = result
+}
+
+function degrees2time(form) {
+    decimal = form.degrees.value
+    hour = Math.floor(decimal)
+    decimal = (decimal - hour) * 60
+    minute = Math.floor(decimal)
+    decimal = (decimal - minute) * 60
+    second = decimal
+    form.timeFromDegrees.value = hour +':'+minute+':'+second
+}
 
 //parallax = angularSeparation(deg2rad(observation[0].ra), deg2rad(observation[0].dec), deg2rad(observation[1].ra), deg2rad(observation[1].dec))
